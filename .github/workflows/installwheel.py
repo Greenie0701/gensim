ea34bf7ef3410424a9fa2f9f56f472f2084b0b9a
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-07-27 23:06:59 +0530

    Update installwheel.py

--- .github/workflows/installwheel.py
+++ .github/workflows/installwheel.py
@@ -22,15 +22,12 @@ def main():
         elif machine in ('aarch64', 'arm64'):
             arch = 'aarch64'
     elif system == 'Windows':
-        if machine in ('aarch64', 'arm64'):
-            arch = 'arm64'
-        else:
+        if machine in ('x86_64', 'amd64'):
             arch = 'amd64'
-     else:
-        if machine in ('aarch64', 'arm64'):
+        else machine in ('arm64', 'ARM64'):
             arch = 'arm64'
-        else:
-            arch = 'amd64'
+    else:
+        arch = 'amd64'
 
     want = f'-cp{vi.major}{vi.minor}-'
     
